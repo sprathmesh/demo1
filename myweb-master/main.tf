4634d7654153b0f5b0e97be6fee3438b4e4676c4
sprathmesh <141143554+sprathmesh@users.noreply.github.com>
2024-08-03 12:12:17 +0530

    Add files via upload
    chin tapk dam dam

---
+++ myweb-master/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+     
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+    region =  "ap-south-1"
+    
+}
+resource "aws_instance" "myec2" {
+  instance_type          = "t2.medium"
+  ami                    = data.aws_ami.app_ami.id
+  
+  
+  tags = {
+    Name = "import"
+  }
+
+}
+resource "aws_instance" "myec2-poll" {
+  instance_type          = "t2.medium"
+  ami                    = data.aws_ami.app_ami.id
+  
+  
+  tags = {
+    Name = "Poll"
+  }
+
+}
+#resource "aws_eip" "lb" {
+ # instance = aws_instance.myec2.id
+#  vpc      = true
+#}
+data "aws_ami" "app_ami" {
+    owners      = ["amazon"]
+    most_recent = true
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10**"]
+  }
+}
---
+++ myweb-master/vars.tf
@@ -0,0 +1,6 @@
+#variable "profile" {
+#  type = string
+#}
+#variable "region" {
+#  type = string
+#}
